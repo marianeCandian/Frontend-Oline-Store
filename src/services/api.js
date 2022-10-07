1fababacded6f13e51557f5dcaae09049b91af8a
alexsousa83 <alex.sousa.araujo@hotmail.com>
2022-10-07 16:56:46 -0300

    Requirement 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 export async function getCategories() {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
-  const json = await fetch(url);
-  const data = await json.json();
+  const jason = await fetch(url);
+  const data = await jason.json();
   return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const url = `https://api.mercadolibre.com/sites/MLB/categories=${categoryId}&q=${query}`;
-  const json = await fetch(url);
-  const data = await json.json();
+  const jason = await fetch(url);
+  const data = await jason.json();
   return data;
 }
